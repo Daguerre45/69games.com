1175f02255460803d0365ec447f55af55be9df8c
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2024-01-07 19:13:55 +0100

    feedback

--- src/routes/feedback.js
+++ src/routes/feedback.js
@@ -2,20 +2,18 @@ var express = require('express');
 var router = express.Router();
 const Feedback = require('../database/models/feedback.model');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('feedback', { title: 'FEEDBACK' });
 });
 
 router.post('/', async function(req, res, next) {
-  const user = req.session.user
-  const feedback = req.body
+  const user = req.session.user.username
+  const feedback = req.body.input-feedback
 
-  // Crear un nuevo usuario
+  
   const newFeedback = new Feedback({user, feedback})
   await newFeedback.save();
 
   console.log('Feedback enviado de usuario: ', user);
-  res.redirect('/feedback'); // Cambia esto según la ruta deseada después del registro
 });
 module.exports = router;
