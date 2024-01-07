eb4e1871d7152ba19f8a744651fcb50787d2824e
Juaki15 <109316446+juaki0315@users.noreply.github.com>
2024-01-07 18:21:21 +0100

    agregado puntos a modelo de usuario

--- src/database/models/user.model.js
+++ src/database/models/user.model.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  points: { type: Number, default: 0 }
 });
 
 const User = mongoose.model('User', userSchema);
