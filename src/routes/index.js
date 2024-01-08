3fa79d45aff8c3ef85dd7e2f6ab132d6e585cf71
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2024-01-08 16:50:38 +0100

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SW1' });
+  res.render('index', { title: '69Games', runnerImageSrc: "/images/runnerGameImage.png", spaceInvadersSrc: "/images/spaceInvadersImage.png", user: req.session.user.username });
 });
 
 module.exports = router;
