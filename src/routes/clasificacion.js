8c4d132f5461305dada0856a3da0e8f9823a7cf9
Juaki15 <109316446+juaki0315@users.noreply.github.com>
2024-01-10 17:31:45 +0100

    clasificacion terminada

--- src/routes/clasificacion.js
+++ src/routes/clasificacion.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../database/models/user.model');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('clasificacion', { title: 'CLASIFICACIÓN' });
+router.get('/', async function(req, res, next) {
+  try {
+    // Obtén la lista de usuarios ordenados por puntos de mayor a menor
+    const users = await User.find().sort({ points: -1 });
+
+    // Renderiza el archivo EJS y pasa la lista de usuarios como variable
+    res.render('clasificacion', { users });
+  } catch (error) {
+    console.error('Error al obtener la clasificación:', error);
+    next(error);
+  }
 });
 
 module.exports = router;
