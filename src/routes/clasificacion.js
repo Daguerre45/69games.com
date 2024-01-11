007df895207fd93e0d7e62a0673fb770a29dcb1c
Juaki15 <109316446+juaki0315@users.noreply.github.com>
2024-01-11 13:14:01 +0100

    correcciones chicas

--- src/routes/clasificacion.js
+++ src/routes/clasificacion.js
@@ -31,6 +31,7 @@ router.get('/', async function (req, res, next) {
 
     // Render the template
     res.render('Clasificacion', {
+      title: "Clasificacion",
       usersRG,
       usersSI,
       profileImageMap,
